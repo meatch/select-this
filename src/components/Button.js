5f3ee78bd30efde262da12281c92cfb2402b478e
Mitch Gohman <mgohman@montrosetravel.com>
2019-10-17 08:01:47 -0700

    Added emotion library for compiled styles to be bundled with NPM package.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from "@emotion/styled";
+import { font, primaryColors, shape } from "config/styles";
+
+const Wrapper = styled.button`
+    ${font}
+    ${primaryColors}
+    ${shape}
+`;
 
 const Button = ({ text, onClick }) => {
-    return <button onClick={ onClick }>{text}</button>;
+    return <Wrapper onClick={ onClick }>{text}</Wrapper>;
 }
 
 export default Button;
