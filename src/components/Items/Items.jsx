d416b6929908c5e4c0bcfe7b89f5cbc370885c4a
Mitchell Gohman <meatch@me.com>
2019-10-19 16:30:34 -0700

    Updated Items to consume Item, which will ultimately be a looped, list of items.

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Item from '../Item/Item.jsx';
+import classnames from 'classnames';
 
 const Items = () => {
+
+    const theClassName = classnames({
+        'Items': true,
+    });
     return (
-        <div>Items</div>
+        <div className={ theClassName }>
+            Items
+            <Item />
+        </div>
     );
 }
 
