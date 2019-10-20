2ec5a6909d7ef3c65c8853ccff4787d1bc5c0d2e
Mitchell Gohman <meatch@me.com>
2019-10-20 10:46:10 -0700

    Managed Default props for HOC.

--- src/SelectHOC.jsx
+++ src/SelectHOC.jsx
@@ -1,60 +1,27 @@
 import React, { useEffect, useRef, useReducer } from 'react';
 import selectContext from './context/selectContext';
 import selectReducer from './context/selectReducer';
+import * as selectActions from './context/selectActions';
+
 import keycode from 'keycode';
 import classnames from 'classnames';
 
 import HiddenInputs from './components/HiddenInputs/HiddenInputs';
+import AriaLabel from './components/AriaLabel/AriaLabel';
 import ButtonDisplay from './components/ButtonDisplay/ButtonDisplay';
 
 const SelectHOC = (WrappedComponent, selectType) => {
-    return (props) => {
-
-        console.log('Props passed', props);
+    const SelectHOCWrapper = (props) => {
 
         const {
-            id=null,
-            additionalClassName = null,
-            
-            // Aria
-            label=null,
-            labelID = null,
-            labelSrOnly = null,
-            
-            /* Items = [
-                { 
-                    # Required Props
-                    uID: 100, //unique identifier for each item, required to keep them distinct
-                    value: 'This is the Value of the Item',
-                    displayText: 'What the User Sees for this Item',
-                    selected: false,
-                    
-                    # Optional Props and their defaults
-                    selectable: true,
-                },
-                ...
-            ]
-            */
-            items = [], //master list of options and their current selected state
-            
-            // Hidden Inputs
-            inputName = 'item', //for hidden input(s) and standard form submission
-            injectHiddenInputs = false,
-
-            // ButtonDisplay
-            buttonDisplayTextDefault = null, //Default text for Toggle Button if nothing is selected
-
-            // Alignment of dropdown list and pyramid that visually connects modal to btnHeroText
-            alignModal = 'right',
-
-            // Return Items State to Parent
-            onSelected = () => { return; },
-
-            // SelectMulti
-            multiMessage = 'Items Selected',
-            min = null,
-            max = null,
-
+            items,
+            id,
+            label, 
+            min, 
+            max,
+            buttonDisplayTextDefault,
+            alignModal,
+            additionalClassName,
         } = props;
 
         /*---------------------------
@@ -77,8 +44,13 @@ const SelectHOC = (WrappedComponent, selectType) => {
             itemsSaved: items,
             buttonDisplayText: (buttonDisplayTextDefault) ? buttonDisplayTextDefault : `Select an item`,
             modalIsOpen: false,
-
-            props: { 
+            focusedItem: {
+                uID: null,
+                item: {},
+            },
+            reachedMax: false,
+            reachedMin: false,
+            originalProps: {
                 ...propsAugmented,
                 ...props
             },
@@ -95,16 +67,27 @@ const SelectHOC = (WrappedComponent, selectType) => {
         const rootClassName = classnames({
             'SelectThis': true,
             [selectType]: true,
+            
+            'modalIsOpen': selectState.modalIsOpen,
+            'reachedMax': selectState.reachedMax,
+            'reachedMin': selectState.reachedMin,
+
+            [`modal-align-${alignModal}`]: true,
+
             [additionalClassName]: additionalClassName,
         });
         const menuClassName = classnames({
             'Menu': true,
         });
 
+        /*---------------------------
+        | Render
+        ---------------------------*/
         return (
             <selectContext.Provider value={ { selectState, dispatch } }>
                 <div id={ id } className={ rootClassName }>
                     <HiddenInputs />
+                    <AriaLabel />
                     <ButtonDisplay />
                     <div className={ menuClassName }>
                         <WrappedComponent { ...props } />
@@ -113,6 +96,57 @@ const SelectHOC = (WrappedComponent, selectType) => {
             </selectContext.Provider>
         );
     }
+
+
+    /*---------------------------
+    | Set Default Props
+    ---------------------------*/
+    SelectHOCWrapper.defaultProps = {
+        id: null,
+        additionalClassName: null,
+        
+        // Aria
+        label: null,
+        ariaLabel: null,
+        labelID: null,
+        labelSrOnly: null,
+        
+        /* Items: [
+            { 
+                # Required Props
+                uID: 100, //unique identifier for each item, required to keep them distinct
+                value: 'This is the Value of the Item',
+                displayText: 'What the User Sees for this Item',
+                selected: false,
+                
+                # Optional Props and their defaults
+                selectable: true,
+            },
+            ...
+        ]
+        */
+        items: [], //master list of options and their current selected state
+        
+        // Hidden Inputs
+        inputName: 'item', //for hidden input(s) and standard form submission
+        injectHiddenInputs: false,
+
+        // ButtonDisplay
+        buttonDisplayTextDefault : null, //Default text for Toggle Button if nothing is selected
+
+        // Alignment of dropdown list and pyramid that visually connects modal to btnHeroText
+        alignModal: 'right',
+
+        // Return Items State to Parent
+        onSelected: () => { return; },
+
+        // SelectMulti
+        multiMessage: 'Items Selected',
+        min: null,
+        max: null,
+    };
+
+    return SelectHOCWrapper;
 }
 
 export default SelectHOC;
